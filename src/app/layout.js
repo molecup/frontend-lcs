1c11788e2f2999c04ba7649ce74a961004dfa32d
mariom8la08 <mario.m8la@gmail.com>
2026-03-17 20:09:19 +0100

    feat: add favicon and update layout for improved site branding

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,12 @@ export default function RootLayout({ children }) {
         <head>
             <title>estudentsleague</title>
             <meta name="description" content="estudentsleague"/>
-            <link rel="icon" href="/favicon.ico"/>
+            <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+            <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <meta name="apple-mobile-web-app-title" content="LCS" />
+            <link rel="manifest" href="/site.webmanifest" />
             <link rel="stylesheet" href="https://use.typekit.net/ajb7nmd.css"/>
             {/* Google Fonts gestiti da next/font/google */}
             <style>{`
